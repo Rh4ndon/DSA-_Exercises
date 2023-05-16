105008c240d899e56ab544767f2e9958db4e002c
Rh4ndon <daverhandon@gmail.com>
2023-05-16 20:20:26 +0800

    Commits on dictrionary data structure

---
+++ NonLinear_DataStructure/Dictionary/Dictionary.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class Dictionary {
+    
+    public static void main(String[] args) {
+// Step 1: Read in a text string
+Scanner scanner = new Scanner(System.in); 
+System.out.print("Enter a text string: "); 
+String text = scanner.nextLine();
+
+
+// Step 2: Split the string into words and convert to lowercase 
+String[] words = text.toLowerCase().split("\\W+");
+
+// Step 3: Create a HashMap to store word frequencies 
+HashMap<String, Integer> frequencies = new HashMap<>();
+// Step 4: Loop over each word and update the frequency count 
+for (String word: words) {
+if (frequencies.containsKey(word)) {
+     frequencies.put(word, frequencies.get(word) + 1); 
+    }
+     else {
+        frequencies.put(word, 1);
+     }
+
+}
+
+
+// Step 5: Print out the word frequencies 
+for (String word : frequencies.keySet()) {
+int frequency = frequencies.get(word);
+System.out.println(word + ":"+ frequency);
+
+}
+
+}
+    
+
+
+}
+
+
